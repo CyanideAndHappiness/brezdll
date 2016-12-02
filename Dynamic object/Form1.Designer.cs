1fe56786b7ed8f87f4967f7be2896eb16cdb6f8c
CyanideAndHappiness <karina__96@list.ru>
2016-12-02 08:21:26 +0300

    Create "Snowman" Add Dynamic object

---
+++ Dynamic object/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Dynamic_object
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SnowmanBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SnowmanBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SnowmanBox
+            // 
+            this.SnowmanBox.Location = new System.Drawing.Point(12, 12);
+            this.SnowmanBox.Name = "SnowmanBox";
+            this.SnowmanBox.Size = new System.Drawing.Size(704, 350);
+            this.SnowmanBox.TabIndex = 0;
+            this.SnowmanBox.TabStop = false;
+            this.SnowmanBox.Paint += new System.Windows.Forms.PaintEventHandler(this.SnowmanBox_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 374);
+            this.Controls.Add(this.SnowmanBox);
+            this.Name = "Form1";
+            this.Text = "Snowman";
+            ((System.ComponentModel.ISupportInitialize)(this.SnowmanBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox SnowmanBox;
+    }
+}
+
---
+++ Dynamic object/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using BrezDraw;
+
+namespace Dynamic_object
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _bitmap;
+        private Graphics _graphics;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            _bitmap = new Bitmap(SnowmanBox.Width, SnowmanBox.Height);
+            _graphics = Graphics.FromImage(_bitmap);
+            SnowmanBox.Resize += SnowmanBox_Resize;
+        }
+
+        private void SnowmanBox_Resize(object sender, EventArgs e)
+        {
+            _bitmap = new Bitmap(SnowmanBox.Width, SnowmanBox.Height);
+            _graphics = Graphics.FromImage(_bitmap);
+        }
+
+        private void SnowmanBox_Paint(object sender, PaintEventArgs e)
+        {
+            
+            var graphics = _graphics;
+            var clr = Color.Black;
+
+            graphics.SmoothingMode = SmoothingMode.HighQuality;
+            graphics.Clear(Color.AliceBlue);
+
+  
+            BrezDraw.Draw.BCircle(_graphics, clr, 120, 75, 35);
+            BrezDraw.Draw.BCircle(_graphics, clr, 120, 155, 45);
+            BrezDraw.Draw.BCircle(_graphics, clr, 120, 270, 70);
+
+            e.Graphics.DrawImage(_bitmap, 0f, 0f);
+
+        }
+    }
+}
