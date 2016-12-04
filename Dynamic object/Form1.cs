212c5920f175a167f3eb91a88c79f89984fa7430
CyanideAndHappiness <karina__96@list.ru>
2016-12-04 14:54:54 +0300

    Создали морковку :D

--- Dynamic object/Form1.cs
+++ Dynamic object/Form1.cs
@@ -17,7 +17,8 @@ namespace Dynamic_object
         private Bitmap _bitmap;
         private Graphics _graphics;
         private Random _random = new Random();
-        private Pen p = new Pen(Color.Black);
+        private Pen blackPen = new Pen(new SolidBrush(Color.Black), 1.5f);
+        private Brush orb = new SolidBrush(Color.Orange);
 
         public Form1()
         {
@@ -33,11 +34,11 @@ namespace Dynamic_object
 
             Timer timer = new Timer();
             timer.Tick += Timer_Tick;
-            timer.Interval = 200;
+            timer.Interval = 50;
             timer.Enabled = true;
 
-         
 
+            
 
         }
 
@@ -60,66 +61,77 @@ namespace Dynamic_object
 
             //e.Graphics.DrawImage(_bitmap, 0f, 0f);
 
-            _graphics.SmoothingMode = SmoothingMode.HighQuality;
-         
+            // _graphics.SmoothingMode = SmoothingMode.HighQuality;
+
 
             e.Graphics.DrawImage(_bitmap, 0f, 0f);
-           
-           
+
+
 
         }
 
         private void SnowmanBox_Paint1(object sender, PaintEventArgs e)
         {
 
-           
+
         }
 
+        private float _vx = 10f;
         private float _x;
-       // private float _y;
-     
-      
+        // private float _y;
 
-        
-        
+
+
+
+
 
         private void Timer_Tick(object sender, EventArgs e)
         {
             //_x += (float)_random.NextDouble() * 2f - 1f;
             //_y += (float)_random.NextDouble() * 2f - 1f;
-        
-                
-            _x += 20f;
-         //   _y += 0f;
+            if (_x + 120 + 95 > SnowmanBox.Width || _x < 0f)
+            {
+                _vx = -_vx;
+            }
+            
+             _x += _vx;
+            //   _y += 0f;
 
-            
-            
-                DrawSnowman(Color.Black);
-           
+
+
+            DrawSnowman(Color.Black);
+
 
             SnowmanBox.Invalidate();
-            
+
 
         }
 
-        
+
 
         private void DrawSnowman(Color clr)
         {
-           
-            
-                _graphics.Clear(Color.Azure);
 
-                Draw.BCircle(_graphics, clr, 120 + _x, 75, 35);
-                Draw.BCircle(_graphics, clr, 120 + _x, 155, 45);
-                Draw.BCircle(_graphics, clr, 120 + _x, 270, 70);
-                Draw.BCircle(_graphics, clr, 103 + _x, 65, 4);
-                Draw.BCircle(_graphics, clr, 130 + _x, 65, 4);
+
+            _graphics.Clear(Color.Azure);
+
+            Draw.BCircle(_graphics, clr, 120 + _x, 75, 35);
+            Draw.BCircle(_graphics, clr, 120 + _x, 155, 45);
+            Draw.BCircle(_graphics, clr, 120 + _x, 270, 70);
+            Draw.BCircle(_graphics, clr, 103 + _x, 65, 4);
+            Draw.BCircle(_graphics, clr, 130 + _x, 65, 4);
+
+          //  var pen = new Pen(new SolidBrush(clr), 1.5f);
+
+            _graphics.DrawLine(blackPen, 120 + _x + 45, 155, 120 + _x + 45 + 50, 155 - 50);
+            _graphics.DrawLine(blackPen, 120 + _x - 45, 155, 120 + _x - 45 - 50, 155 - 50);
+
+            _graphics.FillPolygon(orb, new[] { new PointF(120+_x - 6, 75f), new PointF(120 + _x - 6 + 20f, 75f+5f), new PointF(120 +  _x - 6, 75f+10f) });
 
         }
 
-     
-      
+
+
 
 
     }
